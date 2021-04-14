00f0b36c5142e71dba5cbaec6c3563ab3f3cc621
Prosper <Onyekosorprosper@gmail.com>
2021-04-14 22:53:30 +0100

    working on showing the music view

--- server.js
+++ server.js
@@ -4,20 +4,52 @@ const serveStatic = require('serve-static');
  const app = express();
 
 
-let databasemusic = {
+let databasemusic = [
+  {
 name: "He turned it",
-date: "i4th of april 2021"
-audiourl: "C:\Users\pc\Music\burna-boy-ye-official-video.mp3"
-};
+firstAuthur: "Tye tribbet",
+date: "i4th of april 2021",
+audiourl: "C:\Users\pc\Music\burna-boy-ye-official-video.mp3",
+description: "Edited and written by praise"
+},
+
+{
+name: "Blessings",
+firstAuthur: "James fortune",
+date: "i4th of april 2021",
+audiourl: "C:\Users\pc\Music\ckay-love-nwantiti-remix-ft-joeboy-kuami-eugene-offici.mp3",
+description: "Edited and written by Solo"
+},
+
+{
+name: "Let it rain",
+firstAuthur: "Kirk franklin",
+date: "i4th of april 2021",
+audiourl: "C:\Users\pc\Music\hakuna-matata-the-lion-king-1994.mp3",
+description: "Edited and written by Prevail"
+}
+];
 
  app.use(express.static(path.join(__dirname, "public")));
  app.set("view engine", "ejs");
 
  app.get("/", function(req, res){
-   res.render("home");
+   res.render("home", {
+     name:databasemusic.name,
+     description: databasemusic.description,
+     date: databasemusic.date,
+     audiourl: databasemusic.audiourl,
+     firstAuthur: databasemusic.firstAuthur,
+     databasemusic: databasemusic
+}
+ );
  });
 
 
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
 
 
 
