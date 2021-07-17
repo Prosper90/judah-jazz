a295f0878969714f4b3b8a5e2ac9b71331f0b5ad
Prosper <Onyekosorprosper@gmail.com>
2021-07-17 21:09:40 +0100

    made a change to the way I connected to mongo using mongo client

--- models/adminModel.js
+++ models/adminModel.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/music", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+const MongoClient = require("mongodb").MongoClient;
+ const client = await new MongoClient(process.env.MONGODB_URI || "mongodb://localhost:27017/music", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+ client.connect();
+mongoose.connection.once('open', () => { console.log('MongoDB Connected'); });
+mongoose.connection.on('error', (err) => { console.log('MongoDB connection error: ', err); });
+
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/music", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 
 const adminSchema  = mongoose.Schema({
